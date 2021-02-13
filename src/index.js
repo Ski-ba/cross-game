7843e840be05f6af43b94e9c714fa18f3ae030f0
Skiba-front <58513588+Skiba-front@users.noreply.github.com>
2021-02-13 21:32:57 +0500

    feat: cross-game for two players

---
+++ src/index.js
@@ -0,0 +1,59 @@
+const diamond = `
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+<g fill="#000000"><path d="M12 2L5 12l7 10 7-10z"></path></g>
+</svg>`;
+
+const heart = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+<g fill="#000000">
+  <path
+    d="M18,4c-4-1-6,3-6,3s-2-4-6-3s-4,6-2,8l8,8l8-8C22,10,22,5,18,4z"
+  ></path>
+</g>
+</svg>`
+
+const buttons = document.querySelectorAll('#button')
+let count = 0
+
+
+let valuesArr = [false,false,false,false,false,false,false,false,false]
+
+function win(value) {
+  let bod = document.querySelector('.body')
+  if(value == 'diamond') {
+    bod.classList.add('green')
+  } else {
+    bod.classList.add('red')
+  }
+}
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', () => {
+    if (valuesArr[i] == false) {
+      count++
+
+      if (count % 2 == 1) {
+        buttons[i].insertAdjacentHTML('afterbegin', diamond)
+        valuesArr[i] = 'diamond'
+        for (let i = 0; i < valuesArr.length; i++) {
+          if (valuesArr[i] === 'diamond' && valuesArr[i+1] === 'diamond' && valuesArr[i+2] === 'diamond' || 
+              valuesArr[i] === 'diamond' && valuesArr[i+3] === 'diamond' && valuesArr[i+6] === 'diamond' ||
+              valuesArr[i] === 'diamond' && valuesArr[i+4] === 'diamond' && valuesArr[i+8] === 'diamond' ||
+              valuesArr[i] === 'diamond' && valuesArr[i+2] === 'diamond' && valuesArr[i+4] === 'diamond') {
+            win('diamond')
+          }
+        }
+      } else {
+        buttons[i].insertAdjacentHTML('afterbegin', heart)
+        valuesArr[i] = 'heart'
+        for (let i = 0; i < valuesArr.length; i++) {
+          if (valuesArr[i] === 'heart' && valuesArr[i+1] === 'heart' && valuesArr[i+2] === 'heart' || 
+              valuesArr[i] === 'heart' && valuesArr[i+3] === 'heart' && valuesArr[i+6] === 'heart' ||
+              valuesArr[i] === 'heart' && valuesArr[i+4] === 'heart' && valuesArr[i+8] === 'heart' ||
+              valuesArr[i] === 'heart' && valuesArr[i+2] === 'heart' && valuesArr[i+4] === 'heart') {
+            win('heart')
+          }
+        }
+      }
+    }
+  })
+}
